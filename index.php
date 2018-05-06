f7f3ca2d7112f9e899c3ed6a54b2fa782ca29e6d
yago8397 <yago_rj@hotmail.com>
2018-05-06 17:06:57 -0300

    Faz pagina inicial

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/bootstrap.css" >
+		<link rel="stylesheet" type="text/css" href="estilo.css">		
+		<title>Gaia Shopping</title>
+	</head>
+	<body>
+		<div class="container">	
+
+			<header>
+				<div class="row" style="background-color: #BDBDBD">
+
+					<div class="col-md-2 mt-2">
+						<figure class="figure">
+	        			<img class="img-fluid"  src="imagens/logo.png" alt="Logo da empresa" />
+	      				</figure>
+					</div>
+		      		<div class="col-md-8 mt-3" >
+		      			<form >
+		        		<input class="form-control mr-sm-2 " type="search" placeholder="Qual produto está procurando?" aria-label="Search">	
+		      			</form>
+		      		</div>
+		      		<div class="col-md-2">
+		      		<div class="w-50 h-25 mt-1 ">
+		      			<a class="btn btn btn-info btn-sm " href="">Login</a>
+		      		</div>		      				      		
+		      		<div class="w-50 h-25 mt-3 ">
+		      			<a class="btn btn btn-info btn-sm " href="">Cadastro</a>
+		      		</div>
+		      		</div>
+				</div>
+			</header>			
+			
+			<nav>		
+				<div class="row col-md-10 offset-md-1">				
+				<div class="col-md-2"></div>
+
+					<ul class="nav nav-pills mt-2 p-1 border border-info border-right-0 border-left-0">
+	  				<li class="nav-item mr-1">
+	    			<a class="nav-link btn btn btn-dark btn-sm bg-info p-1" href="#">Início</a>
+					</li>
+					<li class="nav-item dropdown mr-1">
+					<a class="nav-link btn btn-outline-dark btn-sm bg-secondary p-1" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Informática e Telefonia</a>
+					<div class="dropdown-menu bg-secondary">
+					<a class="dropdown-item" href="#">Celulares</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Tablets</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Pc Gaming</a>								
+					</div>
+
+					<li class="nav-item dropdown mr-1">
+					<a class="nav-link btn btn-outline-dark btn-sm bg-secondary p-1" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Eletrodomésticos</a>
+					<div class="dropdown-menu bg-secondary">
+					<a class="dropdown-item" href="#">Tv's</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Ar condicionado</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Áudio</a>								
+					</div>
+
+					<li class="nav-item dropdown mr-1">
+					<a class="nav-link btn btn-outline-dark btn-sm bg-secondary p-1" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Móveis e Decoração</a>
+					<div class="dropdown-menu bg-secondary">
+					<a class="dropdown-item" href="#">Sala</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Cozinha</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Banheiro</a>								
+					</div>
+
+					<li class="nav-item dropdown ">
+					<a class="nav-link btn btn-outline-dark btn-sm bg-secondary p-1" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Outros</a>
+					<div class="dropdown-menu bg-secondary">
+					<a class="dropdown-item" href="#">Jogos</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Filmes</a>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item" href="#">Livros</a>								
+					</div>
+					</ul>																
+				</div>
+			</nav>			
+
+			<section>
+				<div class="col-md-6 offset-md-3">
+					<figure class="figure mt-2 mb-2">
+	        		<img class="img-fluid"  src="imagens/gif.gif" alt="Banner da empresa" />
+	      			</figure>
+				</div>
+
+				<div class="row m-1">
+
+					<div class="col-md-4">
+
+					<div class="card h-100 w-auto" >
+					<img class="card-img-top p-3" src="imagens/iphone.jpg" alt="Card image cap">
+					<div class="card-body bg-info">
+					<h5 class="card-title ">iPhone 7 Plus</h5>
+					<p class="card-text ">128GB Vermelho Tela Retina HD 5,5" 3D Touch Câmera Dupla de 12MP - Apple.</p>
+					</div>
+					<ul class="list-group list-group-flush">
+					<li class="list-group-item bg-info ">De: R$ 4.324,99 </li>
+					<li class="list-group-item bg-info">Por: R$ 3.892,49 no boleto</li>
+					<li class="list-group-item bg-info ">Ou R$ 4.324,99 em até 15x de R$ 288,33 s/ juros</li>
+					</ul>
+					<div class="card-body">
+					<a href="#" class="btn btn-info p-1">Detalhes</a>
+					<a href="#" class="btn btn-info p-1">Comprar</a>
+					</div>
+					</div>
+
+					</div>
+
+					<div class="col-md-4  ">	
+
+					<div class="card h-100 w-auto" >
+					<img class="card-img-top p-3" src="imagens/tv.png" alt="Card image cap">
+					<div class="card-body bg-info">
+					<h5 class="card-title ">Smart TV LED 55" Philco</h5>
+					<p class="card-text">PH55A17DSGWA Full HD com Conversor Digital 3 HDMI 2 USB Wi-Fi.</p>
+					</div>
+					<ul class="list-group list-group-flush">
+					<li class="list-group-item bg-info">De: R$ 2.299,99 </li>
+					<li class="list-group-item bg-info">Por: R$ 2.699,99 no boleto</li>
+					<li class="list-group-item bg-info">Ou R$ 2.299,99 em até 20x de R$ 114,99 s/ juros</li>
+					</ul>
+					<div class="card-body">
+					<a href="#" class="btn btn-info p-1">Detalhes</a>
+					<a href="#" class="btn btn-info p-1">Comprar</a>
+					</div>
+					</div>
+
+					
+					</div>
+
+
+					<div class="col-md-4 ">	
+
+					<div class="card h-100 w-auto" >
+					<img class="card-img-top p-3" src="imagens/tb.jpg" alt="Card image cap">
+					<div class="card-body bg-info">
+					<h5 class="card-title">Tablet Multilaser M7S</h5>
+					<p  class="card-text">8GB Wi-Fi 7" Android 4.4 Quad Core - Preto.</p>
+					</div>
+					<ul class="list-group list-group-flush">
+					<li class="list-group-item bg-info">De: R$ R$ 271,00 </li>
+					<li class="list-group-item bg-info">Por: R$ 3.892,49 no boleto</li>
+					<li class="list-group-item bg-info">Ou R$ 271,00 em até 10x de R$ 27,10 s/ juros</li>
+					</ul>
+					<div class="card-body">
+					<a href="#" class="btn btn-info p-1">Detalhes</a>
+					<a href="#" class="btn btn-info p-1">Comprar</a>
+					</div>
+					</div>
+					</div>
+				</div>					
+			</section>
+
+			<footer>
+				<div class="row">
+					<div class="col-md-12 mt-1">
+						<h5 class="card-header bg-info">GaiaShopping</h5>
+						<div class="card-body" style="background-color: #BDBDBD">
+						<div class="dropdown-divider"></div>					
+						<p class="card-text text-center">Todas as regras e promoções são válidas apenas para produtos vendidos e entregues pela gaiashop.com.<br>				
+						Av. das Américas, 4666 <br> Barra da Tijuca, Rio de Janeiro - RJ, 22640-102</p>
+						<div class="dropdown-divider"></div>						
+						<a href="#" class="btn btn-info col-md-4 offset-md-4">Voltar ao topo</a>
+						</div>
+					</div>
+				</div>
+			</footer>
+
+
+		</div>		
+
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
